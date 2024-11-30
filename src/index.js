a81bd9a92595763a709e7f6ab7d69a39abaa8767
IvanLisun <114064849+IvanLisun@users.noreply.github.com>
2024-11-30 21:51:27 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Для React 18 і вище
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from './redux/store';
+import App from './App';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+// Створюємо кореневий елемент
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Рендеримо додаток з обгортками для Redux і Router
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
+
+// Запускаємо вимірювання продуктивності
+reportWebVitals();
